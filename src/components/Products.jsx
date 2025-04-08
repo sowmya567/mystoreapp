945f821cc99c9c3531dc4bc7985e50c44406c784
Sowmya567 <sowmya22241a05a2@grietcollege.com>
2025-04-08 11:04:29 +0530

    updated

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,26 +1,23 @@
-import "./Products.css"
-export default function Products(){
-    const products=[
-        {id:1, name:"product 1",price:35},
-        {id:2, name:"product 2",price:45},
-        {id:3, name:"product 3",price:55},
-        {id:4, name:"product 4",price:65},
-        {id:5, name:"product 5",price:75},
-        {id:6, name:"product 6",price:85},
-    
-    ];
-    return(
-        <div >
-            <h2>Product List</h2>
-            <div className="App-Products-row">
-                {products.map((value,index)=>(
-                    <div className="App-Products-Box" key={index}>
-                        <h3>{value.name}</h3>
-                        <h4>{value.price}</h4>
-                        <button>Add to cart</button>
-                        </div>
-            ))}
-            </div>
-        </div>
-    )
+import "./Products.css";
+import { appContext } from "../App";
+import { useContext } from "react";
+export default function Products() {
+  const { user, products, cart, setCart } = useContext(appContext);
+  const addToCart = (id) => {
+    setCart({ ...cart, [id]: 1 });
+  };
+  return (
+    <div>
+      {user.name}
+      <div className="App-Products-Row">
+        {products.map((value, index) => (
+          <div className="App-Products-Box" key={index}>
+            <h3>{value.name}</h3>
+            <h4>{value.price}</h4>
+            <button onClick={() => addToCart(value.id)}>Add to Cart</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
